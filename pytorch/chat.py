1245000de55d8842dae8eae16e1bc3528f2ec694
Guenter Bartsch <guenter@zamia.org>
2019-09-28 22:27:22 +0200

    minimum context, interactive chat loop

--- pytorch/chat.py
+++ pytorch/chat.py
@@ -5,7 +5,7 @@ import os
 import sys
 import json
 
-# import readline
+import readline
 # from pathlib import Path
 # from lm import inference
 
@@ -17,15 +17,8 @@ import torch
 from mem_transformer import MemTransformerLM
 from inference import ModelWrapper
 
-# MODEL_PATH = 'de61M-root'
-# SP_MODEL_PATH = '../sp-model.model'
-
-# MODEL_PATH = 'de61M-base-small-root'
-# SP_MODEL_PATH = '../sp-model.model'
-
-MODEL_PATH = 'de144M-base-small-250k-root'
-SP_MODEL_PATH = '../de-base-250k-sp-model.model'
-
+MODEL_PATH = 'de163M-base-root'
+SP_MODEL_PATH = 'de163M-base-root/sp-model.model'
 
 DEVICE = 'cuda'
 
@@ -39,71 +32,58 @@ TOP_K = 8
 # load model
 #
 
+print ("loading model from %s ..." % MODEL_PATH)
+
 mw = ModelWrapper.load(MODEL_PATH, SP_MODEL_PATH, DEVICE)
 
 # txt = "Die Forschung an der künstlichen Intelligenz"
-txt = "Die Hitzewelle, mit der sich die amerikanische Metropole New York derzeit rumschlägt, wird langsam zum immer größeren Problem: Bei Temperaturen von 40 Grad waren rund 50.000 Haushalte in Brooklyn und in Westchester County am Sonntag ohne Strom. Das teilten"
+# txt = "Die Hitzewelle, mit der sich die amerikanische Metropole New York derzeit rumschlägt, wird langsam zum immer größeren Problem: Bei Temperaturen von 40 Grad waren rund 50.000 Haushalte in Brooklyn und in Westchester County am Sonntag ohne Strom. Das teilten"
 
+while True:
 
-tokens = mw.tokenize(txt)
+    txt = input("(q: quit) > ") 
 
-print(tokens)
-
-# lp = mw.get_log_probs(tokens)
-# 
-# print(lp)
-# 
-# for idx in range(len(tokens)):
-#     log_probs = mw.get_log_probs(tokens)[idx]
-#     top_indices = torch.argsort(log_probs)[-TOP_K:]
-#     top_log_probs = log_probs[top_indices]
-# 
-#     top_k_log_probs = [(log_prob.item(), mw.sp_model.IdToPiece(idx.item()))
-#                       for idx, log_prob in
-#                       reversed(list(zip(top_indices, top_log_probs)))]
-# 
-#     print ("%2d: %s -> %s" % (idx, tokens[idx], top_k_log_probs))
-# 
-# 
-# sys.exit(0)
-
- 
-# ntk = mw.next_top_k(tokens, TOP_K)
-# 
-# print (ntk)
-
-for i in range(TOKENS_TO_GENERATE):
-
-    # generate TOP_K potential next tokens
-    ntk = mw.next_top_k(tokens, TOP_K)
-    # ntk = mw.get_next_top_k(tokens, TOP_K)
-
-    # print (ntk)
-
-    # import pdb; pdb.set_trace()
-
-    # convert log probs to real probs
-    logprobs = np.array(list(map(lambda a: a[0], ntk)))
-    probs = np.exp(logprobs) / np.exp(logprobs).sum()
-
-    # pick next token randomly according to probs distribution
-    next_token_n = np.random.choice(TOP_K, p=probs)
-    next_token = ntk[next_token_n][1]
-    # print (next_token)
-   
-    tokens.append(next_token)
-
-    if next_token[0] == '▁':
-        sys.stdout.write(' ')
-    sys.stdout.write(mw.sp_model.DecodePieces([next_token]))
-    sys.stdout.flush()
-
-    # find a good point to stop
-    if (i > MIN_TOKENS_TO_GENERATE) and \
-       ( ('.' in next_token) or ('!' in next_token) or ('?' in next_token) ):
+    if txt == 'q':
         break
 
-print()
+    tokens = mw.tokenize(txt)
+    print(tokens)
+    if len(tokens)<20:
+        print ("not enough context, please try again")
+        continue
+
+    for i in range(TOKENS_TO_GENERATE):
+
+        # generate TOP_K potential next tokens
+        ntk = mw.next_top_k(tokens, TOP_K)
+        # ntk = mw.get_next_top_k(tokens, TOP_K)
+
+        # print (ntk)
+
+        # import pdb; pdb.set_trace()
+
+        # convert log probs to real probs
+        logprobs = np.array(list(map(lambda a: a[0], ntk)))
+        probs = np.exp(logprobs) / np.exp(logprobs).sum()
+
+        # pick next token randomly according to probs distribution
+        next_token_n = np.random.choice(TOP_K, p=probs)
+        next_token = ntk[next_token_n][1]
+        # print (next_token)
+       
+        tokens.append(next_token)
+
+        if next_token[0] == '▁':
+            sys.stdout.write(' ')
+        sys.stdout.write(mw.sp_model.DecodePieces([next_token]))
+        sys.stdout.flush()
+
+        # find a good point to stop
+        # if (i > MIN_TOKENS_TO_GENERATE) and \
+        #    ( ('.' in next_token) or ('!' in next_token) or ('?' in next_token) ):
+        #     break
+
+    print()
 
 sys.exit(0)
 
@@ -154,3 +134,20 @@ while True:
 
 #print(mw.sp_model.DecodePieces(tokens))
 
+# lp = mw.get_log_probs(tokens)
+# 
+# print(lp)
+# 
+# for idx in range(len(tokens)):
+#     log_probs = mw.get_log_probs(tokens)[idx]
+#     top_indices = torch.argsort(log_probs)[-TOP_K:]
+#     top_log_probs = log_probs[top_indices]
+# 
+#     top_k_log_probs = [(log_prob.item(), mw.sp_model.IdToPiece(idx.item()))
+#                       for idx, log_prob in
+#                       reversed(list(zip(top_indices, top_log_probs)))]
+# 
+#     print ("%2d: %s -> %s" % (idx, tokens[idx], top_k_log_probs))
+# 
+# 
+# sys.exit(0)
